e2bd863b5704815e798ce7db062b3727d210c771
incorerct <tuvshuut0221@gmail.com>
2024-12-24 20:50:26 +0800

    Updates

---
+++ precache-manifest.ae6e6edfc8f2423f725c403343d8db74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c9af09d7f540241c7c2ad4d8e72a2e8",
+    "url": "/medee/index.html"
+  },
+  {
+    "revision": "5843f6c470df3c5cd66d",
+    "url": "/medee/static/css/main.63448fb7.chunk.css"
+  },
+  {
+    "revision": "60c2f8d9b8ef97bf6b1b",
+    "url": "/medee/static/js/2.70ea85ee.chunk.js"
+  },
+  {
+    "revision": "4eb5a98fbfbd8a5a5716ab19c411c0bf",
+    "url": "/medee/static/js/2.70ea85ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee20ad26bfeee310d5e0",
+    "url": "/medee/static/js/3.92613fef.chunk.js"
+  },
+  {
+    "revision": "5843f6c470df3c5cd66d",
+    "url": "/medee/static/js/main.566b2f1c.chunk.js"
+  },
+  {
+    "revision": "78af5429fb74f2bb4797",
+    "url": "/medee/static/js/runtime-main.e8a6dfd7.js"
+  }
+]);
